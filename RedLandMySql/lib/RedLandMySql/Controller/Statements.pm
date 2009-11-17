11b504c478871016fce569d6b9b86b523178f560
James Michael DuPont <mdupont@introspector-desktop.(none)>
2009-11-17 16:48:08 +0100

    perl is working

--- RedLandMySql/lib/RedLandMySql/Controller/Statements.pm
+++ RedLandMySql/lib/RedLandMySql/Controller/Statements.pm
@@ -1,10 +1,27 @@
+
+
+package Error;
+sub type {
+    return "return";
+}
+1;
+
+
 package RedLandMySql::Controller::Statements;
+
+sub mycatch
+{
+    my ($context,$error,$output);
+    warn "Big problem $error";
+    my $exception = { error => $error };
+    bless $exception,Error;
+}
 
 use strict;
 use warnings;
 use parent 'Catalyst::Controller';
 use Data::Dumper;
-
+use Template::Stash;
 =head1 NAME
 
 RedLandMySql::Controller::Statements - Catalyst Controller
@@ -122,7 +139,7 @@ sub subjects :   Path('/statements/subjects')    Args(1)
     $c->stash->{template} = 'statements/list.tt';
 
 
-    DumpResults    ($self, $c);
+#    DumpResults    ($self, $c);
 
 }
 
@@ -183,6 +200,134 @@ sub object :   Path('/statements/object')    Args(1)
 
     $c->stash->{template} = 'statements/list.tt';
 
+}
+
+
+sub TestTemplate :   Path('/statements/template')    Args(1) 
+{
+    my $self=shift; # the catalyst object
+    my $context = shift || die "template sub called without context\n";
+    my $sid=shift;
+
+    $context->stash->{statements} = [
+	$context->model('DB::Statements17546201007601059027')->search(
+	    {
+		subject => $sid 		
+	    }
+	)
+	];
+
+    my $stash = Template::Stash->new($context->{stash});
+    
+    #my $stash   = ;
+    my $output  = 'Hello template';
+    my $_tt_error;
+    
+#    eval { 
+
+	$output .= "Inside the template";
+
+#      BLOCK: {
+
+#	  $output .=  $context->debugging('msg', { 'line' => '1', 'text' => 'META title = \'Statement List\'', 'file' => 'internalized-template' }); ## DEBUG ##
+	  
+	  $output .=  "    ";
+
+#	  $output .=  $context->debugging('msg', { 'line' => '2', 'text' => 'USE Dumper;', 'file' => 'internalized-template' }); ## DEBUG ##
+
+# USE
+#	  $stash->set('Dumper',		      $context->plugin('Dumper'));
+	  
+	  $output .=  "\n  <table>\n  <tr><th>Subject</th><th>Predicate</th><th>Object</th></tr>\n\n  ";
+
+#	  $output .=  $context->debugging('msg', { 'line' => '3', 'text' => 'FOREACH statement IN statements', 'file' => 'internalized-template' }); ## DEBUG ##
+
+	  
+# FOREACH 
+#	  do {
+	      my ($_tt_value, $_tt_error, $_tt_oldloop);
+	      my $_tt_list = $stash->{statements};;
+	      
+	      unless (UNIVERSAL::isa($_tt_list, 'Template::Iterator')) {
+		  $_tt_list = Template::Config->iterator($_tt_list)
+		      || die $Template::Config::ERROR, "\n"; 	      }
+	      
+	      ($_tt_value, $_tt_error) = $_tt_list->get_first();
+	      eval { $_tt_oldloop = $stash->get('loop') };
+	      $stash->set('loop', $_tt_list);
+	     
+
+#	      eval {
+		#FOR1: 
+		  while (! $_tt_error) {
+		      $stash->{'statement'} = $_tt_value;
+		      $output .=  "  <tr>\n    <td>\n      ";
+#		      $output .=  $context->debugging('msg', { 'line' => '4', 'text' => '#Dumper.dump(statement)', 'file' => 'internalized-template' }); ## DEBUG ##
+	      
+		      $output .=  "\n      ";
+#		      $output .=  $context->debugging('msg', { 'line' => '5', 'text' => 'FOREACH statementor IN statement.object_resources', 'file' => 'internalized-template'} ); ## DEBUG ##
+		    
+# FOREACH 
+#		    do {
+			my ($_tt_value, $_tt_error, $_tt_oldloop);
+			my $_tt_list = $stash->get(['statement', 0, 'object_resources', 0]);
+			
+			unless (UNIVERSAL::isa($_tt_list, 'Template::Iterator')) {
+			    $_tt_list = Template::Config->iterator($_tt_list)
+				|| die $Template::Config::ERROR, "\n"; 
+			}
+			
+			($_tt_value, $_tt_error) = $_tt_list->get_first();
+			eval { $_tt_oldloop = $stash->get('loop') };
+			$stash->set('loop', $_tt_list);
+#			eval {
+			    #	  FOR2:   
+			    while (! $_tt_error) {
+				$stash->{'statementor'} = $_tt_value;
+				$output .=  "      ";
+				
+#				$output .=  $context->debugging('msg', { 'line' => '6', 'text' => 'statementor.uri', 'file' => 'internalized-template' }); ## DEBUG ##
+				
+				$output .=  $stash->get(['statementor', 0, 'uri', 0]);
+				$output .=  "\n      ";
+				
+#				$output .=  $context->debugging('msg', { 'line' => '7', 'text' => 'END', 'file' => 'internalized-template' }); ## DEBUG ##;
+				($_tt_value, $_tt_error) = $_tt_list->get_next();
+			    }
+#			};
+			$stash->set('loop', $_tt_oldloop);
+			die $@ if $@;
+			$_tt_error = 0 if $_tt_error && $_tt_error eq Template::Constants::STATUS_DONE;
+			die $_tt_error if $_tt_error;
+#		    }; # do 
+		      
+		      my $x;
+		      
+		      $output .=  "\n    <td></td>\n  </tr>\n  ";
+		      ($_tt_value, $_tt_error) = $_tt_list->get_next();
+		  }# while not error
+#	      }; # eval
+
+	      $stash->set('loop', $_tt_oldloop);
+	      die $@ if $@;
+	      $_tt_error = 0 if $_tt_error && $_tt_error eq Template::Constants::STATUS_DONE;
+	      die $_tt_error if $_tt_error;
+#	  }; # do 
+	
+	  $output .=  "\n</table>\n";
+#	}
+#}
+    if ($@) {
+        $_tt_error = mycatch($context,$@, \$output);
+        die $_tt_error unless $_tt_error->type eq 'return';
+    }
+    
+    
+    $context->response->body(  $output );
+ #   $stash->{'message'} =  $output ;
+#    print $output;
+
+    return $output;
 }
 
 
